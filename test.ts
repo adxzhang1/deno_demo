7a667b0672ac7c9450704d8d2a702ed3164aebd5
alex <azalexanderzhang@gmail.com>
2021-08-03 00:50:41 -0400

    added log and demo file

--- test.ts
+++ test.ts
@@ -1,5 +1,10 @@
 import * as path from 'https://deno.land/std@0.99.0/path/mod.ts';
 
 const file = path.join(Deno.cwd(), 'test.js');
+console.log(file);
 
-console.log(await import(file));
+try {
+  console.log(await import(file));
+} catch (err) {
+  console.log(err);
+}
